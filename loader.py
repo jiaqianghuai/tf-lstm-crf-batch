9d972a3d279402849e730dea32a64a7a6a29396a
贾强槐 <hzjiaqianghuai@corp.netease.com>
2017-07-23 13:50:08 +0800

    fix bug

--- loader.py
+++ loader.py
@@ -3,8 +3,6 @@ import sys
 import re
 import codecs
 from utils import create_dico, create_mapping, zero_digits
-from utils import iob2, iob_iobes
-
 
 def load_sentences(path, lower, zeros):
     """
